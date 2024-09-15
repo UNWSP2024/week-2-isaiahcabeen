204aa55225c44b371d03a54ec9565a1605d8f3bb
isaiahcabeen <157175009+isaiahcabeen@users.noreply.github.com>
2024-09-14 22:17:35 -0500

    Isaiah Cabeen, (9/10), Personal Information
    I did it like Tuesday but I think I submitted it the wrong week

--- program_1.py
+++ program_1.py
@@ -1,6 +1,10 @@
 def personal_information():
-    print('...')
-
-
-# Line which calls the above function.
+    print("Isaiah Cabeen")
+    # Name
+print("2004 13th Ave NE, Austin, MN, 55912")
+#address
+print("651-802-6907")
+#Number
+print("Computer Science")
+#major
 personal_information()
